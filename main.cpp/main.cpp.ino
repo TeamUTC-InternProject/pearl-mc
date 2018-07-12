c74374c432af1825939fca4a5132679ea1e554f0
Anthony Verghese <anthony.verghese@gmail.com>
2018-07-12 16:10:31 -0400

    Adding main.cpp file

---
+++ main.cpp/main.cpp.ino
@@ -0,0 +1,12 @@
+#include <Passive.h>
+Passive passive();
+
+void setup() {
+  // put your setup code here, to run once:
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  passive.sdWrite(1);
+}
