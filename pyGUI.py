51f5ffd4de1a2bfa961a6e630e6f0648aa8bb85f
Chris Evers <chrise92@vt.edu>
2018-07-25 18:20:28 -0400

    fixed str encoding for PySerial

--- pyGUI.py
+++ pyGUI.py
@@ -4,14 +4,14 @@ import serial
 
 arduinoData = serial.Serial('com5',9600)
 
-# LED_ON = byte(1)
-# LED_OFF = byte(2)
+LED_ON = str.encode('1')
+LED_OFF = str.encode('2')
 
 def led_on():
-    arduinoData.write('1')
+    arduinoData.write(LED_ON)
 
 def led_off():
-    arduinoData.write('2')
+    arduinoData.write(LED_OFF)
 
 led_control_window = tkinter.Tk()
 Button = tkinter.Button
@@ -20,3 +20,4 @@ btn1 = Button(led_control_window, text="Passive", command=led_off)
 btn.grid(row=0,column=1)
 btn1.grid(row=1, column=1)
 led_control_window.mainloop()
+
